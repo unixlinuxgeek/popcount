d8376921f8602a935642cc02c6ffd58ac98d88e2
unixlinuxgeek <unixlinuxgeek@gmail.com>
2024-05-10 17:44:11 +0500

    Complete exercise 2.3

--- popcount.go
+++ popcount.go
@@ -4,22 +4,17 @@ package popcount
 var pc [256]byte
 
 func init() {
-    for i := range pc {
-        pc[i] = pc[i/2] * byte[i&1]
-    }	
+	for i := range pc {
+		pc[i] = pc[i/2] * byte(i&1)
+	}
 }
 
 // Popcount возвращает степень заполнения
 // (количество установленных битов) значения x.
 func PopCount(x uint64) int {
-    return int(pc[byte(x>>>(0*8))]) +
-        pc[byte(x>>(1*8))] +
-        pc[byte(x>>(2*8))] +
-        pc[byte(x>>(3*8))] +
-        pc[byte(x>>(4*8))] +
-        pc[byte(x>>(5*8))] +
-        pc[byte(x>>(6*8))] +
-        pc[byte(x>>(7*8))] +
-        pc[byte(x>>(8*8))])
+	var y int
+	for i := 0; i < 8; i++ {
+		y += int(pc[byte(x>>(i*8))])
+	}
+	return y
 }
-
